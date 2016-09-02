bd0273aeebb9ef9a18191bbc9f0b530e2e679826
markzhai <mark.zhai@morecruit.cn>
2016-09-02 15:04:32 +0800

    add wrongly removed dummy file, add license

--- adapter/src/main/java/com/github/markzhai/recyclerview/Dummy.java
+++ adapter/src/main/java/com/github/markzhai/recyclerview/Dummy.java
@@ -15,23 +15,5 @@
  */
 package com.github.markzhai.recyclerview;
 
-import android.databinding.ViewDataBinding;
-import android.support.v7.widget.RecyclerView;
-
-/**
- * @author markzhai on 16/3/18
- * @version 1.0.0
- */
-public class BindingViewHolder<T extends ViewDataBinding> extends RecyclerView.ViewHolder {
-
-    protected final T mBinding;
-
-    public BindingViewHolder(T binding) {
-        super(binding.getRoot());
-        mBinding = binding;
-    }
-
-    public T getBinding() {
-        return mBinding;
-    }
+public class Dummy {
 }
